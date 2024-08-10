7e4364ecbc611b4842f05ec34dd81c3df2e11938
Benjamin Moloise Mishra <benjaminmishra@gmail.com>
2024-08-10 22:46:25 +0200

    Initial Commit

---
+++ src/AppointmentSystem.Api/Endpoints.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppointmentSystem.Api;
+
+public static class Endpoints
+{
+    public static void RegisterEndpoints(this IEndpointRouteBuilder endpointRouteBuilder)
+    {
+        var calendarGroup = endpointRouteBuilder.MapGroup("/calendar");
+
+        calendarGroup
+        .MapPost("query",QueryHandler)
+        .WithName("Query")
+        .WithOpenApi();
+
+    }
+
+    private static ActionResult QueryHandler()
+    {
+        return new OkResult();
+    }
+}
