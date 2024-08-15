0be289bf3d542316868e1354056f8a4f0436009c
Benjamin Moloise Mishra <benjaminmishra@gmail.com>
2024-08-15 23:11:33 +0200

    Created application project

---
+++ src/AppointmentSystem.Application/GetAvailableSlotsQuery.cs
@@ -0,0 +1,21 @@
+﻿namespace AppointmentSystem.Application;
+
+public class GetAvailableSlotsQuery
+{
+    private async Task<IEnumerable<AvailableSlot>> GetAvailableSlots(string[] languages, string[] products, string[] ratings, CancellationToken cancellationToken)
+    {
+        await using var dbConnection = new NpgsqlConnection(_connectionString);
+        var sql = """
+                      SELECT * 
+                      FROM fn_matching_available_slots(@langs, @Ratings, @Prods);
+                  """;
+        var command = new CommandDefinition(sql, new
+        {
+            Langs = languages,
+            Ratings = ratings,
+            Prods = products
+        }, cancellationToken: cancellationToken);
+        
+        return await dbConnection.QueryAsync<AvailableSlot>(command);
+    }
+}
